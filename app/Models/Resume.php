4fa218b843ea585612efa6f88332b793ea099110
Jerome Mendoza <161692292+Emetetsu@users.noreply.github.com>
2024-11-19 19:27:20 +0800

    Update Resume.php

--- app/Models/Resume.php
+++ app/Models/Resume.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Resume extends Model
 {
-    //
+    use HasFactory;
+
+    // Define the table name (if it's not the default 'resumes')
+    protected $table = 'resumes';
+
+    // Define the columns you want to be mass assignable
+    protected $fillable = ['name', 'email', 'phone', 'about', 'education'];
 }
