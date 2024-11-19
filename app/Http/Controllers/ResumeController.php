ddfbe1fcd105e8583fb0e624e165923a1c4a5a90
Jerome Mendoza <161692292+Emetetsu@users.noreply.github.com>
2024-11-19 19:17:13 +0800

    Update ResumeController.php

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Resume; // Use the Resume model
 use Illuminate\Http\Request;
 
 class ResumeController extends Controller
 {
-    //
+    public function showResume()
+    {
+        // Fetch resume data from the database (you might need to adjust this)
+        $resume = Resume::first(); // or use any appropriate query to fetch data
+        
+        return view('resume', compact('resume')); // Pass resume data to the view
+    }
 }
